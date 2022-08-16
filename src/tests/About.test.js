35989a7b27c7cc2c2f3b85f14417f6cb62c49a9f
Edson Pereira <ed.1206@gmail.com>
2022-08-16 18:03:53 -0300

    Resolvido requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,63 @@
-test(' ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test the Home component', () => {
+  test('Test if the top of the application contains the Home link', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('Test if the application is redirected to the Home page', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+});
+
+describe('Test the About component', () => {
+  test('Test if the top of the application contains the About link', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('Test if the application is redirected to the About page', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const aboutText = screen.getByText('About Pokédex');
+    expect(aboutText).toBeInTheDocument();
+    const aboutImage = screen.getByAltText('Pokédex');
+    expect(aboutImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
+
+describe('Test the Favorite Pokemons component', () => {
+  test('Test if the top of the application contains the Favorite Pokémons link', () => {
+    renderWithRouter(<App />);
+    const favPokemonsLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favPokemonsLink).toBeInTheDocument();
+  });
+  test('Test if the application is redirected to the Favorite Pokemons page', () => {
+    const { history } = renderWithRouter(<App />);
+    const favPokemonsLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favPokemonsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
+
+describe('Test the Not Found component', () => {
+  test('Test if the application is redirected to the Not Found page', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/trybeCSS');
+    const noFoundTitle = screen.getByRole('heading', { level: 2, name: /not found/i });
+    expect(noFoundTitle).toBeInTheDocument();
+  });
+});
